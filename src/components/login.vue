e793035eda464c1436e1ba8f00dd451d0641305f
xujing <754130935@qq.com>
2019-12-19 10:11:11 +0800

    登录初始化

---
+++ src/components/login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      登录组件
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  created(){
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
